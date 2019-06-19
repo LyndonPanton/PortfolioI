12528a199e61818da2358abf19b705a1d3c2ebfb
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-19 18:25:32 +0100

    Removed wanted code and comments

--- js/app.js
+++ js/app.js
@@ -2,14 +2,11 @@ window.onload = function() {
 	document.getElementById("copyright-year").textContent = (new Date().getFullYear());
 
 	function scrollDownEvent(event) {
-		// console.log(window.scrollY);
 		let header = document.getElementById("header");
-		if (window.scrollY > 150) {
-			// document.getElementById("header").style.opacity = 0.9;
-			header.classList.add("header-transparent");
 
+		if (window.scrollY > 150) {
+			header.classList.add("header-transparent");
 		} else {
-			// document.getElementById("header").style.opacity = 1.0;
 			header.classList.remove("header-transparent");
 		}
 	}
